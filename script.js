ff8ae135e58aaaf1adeafae592654cd4f4e17872
nitesh8652 <niteshsalian8652085233>
2024-12-27 21:21:11 +0530

    adding more functionalities

---
+++ script.js
@@ -0,0 +1,36 @@
+let input = document.querySelector(".input");
+let copy = document.querySelector(".copy");
+let generate = document.querySelector(".generate");
+
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "!@#$%^&*()_+";
+
+generate.addEventListener("click", () => {
+    let pass = "";
+    let all = uppercase + lowercase + number + symbol;
+    for (let i = 0; i < 15; i++) {
+        let char = Math.floor(Math.random() * all.length);
+        pass += all.charAt(char);
+    }
+    input.value = pass;
+});
+
+
+
+copy.addEventListener("click", () => {
+    let pass = input.value;
+    if (pass){
+        navigator.clipboard.writeText(pass)
+        .then(()=>{
+            console.log("Password copied to clipboard");
+        })
+        .catch((err)=>{
+            console.log("failed to copy",err);
+        })
+    }else{
+        console.log("No password to copy");
+    }
+});
